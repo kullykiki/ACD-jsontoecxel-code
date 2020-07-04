c5f6a1b54946356f7c723dbe461247e58812421c
Kully <nexus5onlykully@gmail.com>
2020-07-04 18:03:49 +0700

    In jsonToexcel.py file add acd_report_2019 list

--- jsonToexcel.py
+++ jsonToexcel.py
@@ -1,30 +1,30 @@
 import pandas
 import os
 import xlrd as xl
-# acd_report =    [   'ACD_january2019',
-#                     'ACD_february2019',
-#                     'ACD_march2019',
-#                     'ACD_april2019',
-#                     'ACD_may2019',
-#                     'ACD_june2019',
-#                     'ACD_july2019',
-#                     'ACD_august2019',
-#                     'ACD_september2019',
-#                     'ACD_october2019',
-#                     'ACD_november2019',
-#                     'ACD_december2019',
-#                 ]
+acd_report_2019 =   [   'ACD_january2019',
+                        'ACD_february2019',
+                        'ACD_march2019',
+                        'ACD_april2019',
+                        'ACD_may2019',
+                        'ACD_june2019',
+                        'ACD_july2019',
+                        'ACD_august2019',
+                        'ACD_september2019',
+                        'ACD_october2019',
+                        'ACD_november2019',
+                        'ACD_december2019',
+                    ]
 
-acd_report =    [   'ACD_january2020',
-                    'ACD_february2020',
-                    'ACD_march2020',
-                    'ACD_april2020',
-                    'ACD_may2020',
-                    'ACD_june2020',
-                    'ACD_july2020'
-                ]
+acd_report_2020 =       [   'ACD_january2020',
+                            'ACD_february2020',
+                            'ACD_march2020',
+                            'ACD_april2020',
+                            'ACD_may2020',
+                            'ACD_june2020',
+                            'ACD_july2020'
+                        ]
 
-for file in acd_report:
+for file in acd_report_2020:
     json_path = r"C:\Users\kully\Desktop\ACD_jsontoexcel_code\json file\2020"
     xlsx_path = r"C:\Users\kully\Desktop\ACD_jsontoexcel_code\excel file\2020"
     json_file = file  + '.json'
