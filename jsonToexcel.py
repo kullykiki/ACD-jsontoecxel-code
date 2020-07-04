196879308c0ce1d21bf5729332ab6c3af327fbb3
Kully <nexus5onlykully@gmail.com>
2020-07-04 14:10:08 +0700

    change type file jsonToexcel use Pandas and forloop to transform multifile and use os to assume path to read and save file

--- jsonToexcel.py
+++ jsonToexcel.py
@@ -1,21 +1,31 @@
 import pandas
+import os
+# acd_report =    [   'ACD_january2019',
+#                     'ACD_february2019',
+#                     'ACD_march2019',
+#                     'ACD_april2019',
+#                     'ACD_may2019',
+#                     'ACD_june2019',
+#                     'ACD_july2019',
+#                     'ACD_august2019',
+#                     'ACD_september2019',
+#                     'ACD_october2019',
+#                     'ACD_november2019',
+#                     'ACD_december2019',
+#                 ]
 
-acd_report =    [   'ACD_january2019',
-                    'ACD_february2019',
-                    'ACD_march2019',
-                    'ACD_april2019',
-                    'ACD_may2019',
-                    'ACD_june2019',
-                    'ACD_july2019',
-                    'ACD_august2019',
-                    'ACD_september2019',
-                    'ACD_october2019',
-                    'ACD_november2019',
-                    'ACD_december2019',
+acd_report =    [   'ACD_january2020',
+                    'ACD_february2020',
+                    'ACD_march2020',
+                    'ACD_april2020',
+                    'ACD_may2020',
+                    'ACD_june2020',
+                    'ACD_july2020'
                 ]
 
 for file in acd_report:
-    file_js = '"./json file/' + file + ".json" + '"'
-    file_xlsx = '"./excel file/' + file + ".xlsx" + '"'
-    # pandas.read_json("ACD_january2020.json").to_excel("ACD_january2020.xlsx", index=False)
-    print(f'command : pandas.read_json({file_js}).to_excel({file_xlsx}, index=False)')
+    json_path = r"C:\Users\kully\Desktop\ACD_jsontoexcel_code\json file\2020"
+    xlsx_path = r"C:\Users\kully\Desktop\ACD_jsontoexcel_code\excel file\2020"
+    json_file = file  + '.json'
+    xlsx_file =  file + '.xlsx'
+    pandas.read_json(os.path.join(json_path, json_file)).to_excel(os.path.join(xlsx_path, xlsx_file), index=False)
